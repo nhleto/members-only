98cd4ff6ea5c1168d2ed014c629b4dcd9eecf974
Henry <hnr.leto11@gmail.com>
2020-10-20 15:26:46 -0400

    create post views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+  # before_action :authenticate_user!, except: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.build(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
